3113835410357de446fc5384e2f5fdf163220226
Jared Franzone <jaredfranzone@Jareds-MacBook-Pro.local>
2023-07-30 16:12:42 -0700

    init

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,57 @@
+
+
+const amplifyconfig = '''{
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+         
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_1vph21DjX",
+                        "AppClientId": "4frfkri1535dldem3bh3bgfvue",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "usernameAttributes": [
+                            "EMAIL"
+                        ],
+                          "signupAttributes": [
+                            "EMAIL"
+                        ],
+                          "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": [
+                                "REQUIRES_LOWERCASE",
+                                "REQUIRES_NUMBERS",
+                                "REQUIRES_SYMBOLS",
+                                "REQUIRES_UPPERCASE"
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+    },
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                 "Endpoint": {
+                     "endpointType": "REST",
+                     "endpoint": "https://i1x4l94mh0.execute-api.us-east-1.amazonaws.com/prod",
+                     "region": "us-east-1",
+                     "authorizationType": "AMAZON_COGNITO_USER_POOLS"
+                    }    
+            }
+        }
+    }
+}''';
---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'amplifyconfiguration.dart';
+
+//
+
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _configureAmplify();
+  runApp(MyApp());
+}
+
+Future<void> _configureAmplify() async {
+  await Amplify.addPlugins([
+    AmplifyAuthCognito(
+      secureStorageFactory: AmplifySecureStorage.factoryFrom(
+        macOSOptions:
+            // ignore: invalid_use_of_visible_for_testing_member
+            MacOSSecureStorageOptions(useDataProtection: false),
+      ),
+    ),
+    AmplifyAPI()
+  ]);
+  try {
+    await Amplify.configure(amplifyconfig);
+  } on AmplifyAlreadyConfiguredException {
+    print(
+        "Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
+  } catch (e) {
+    print("An error occurred while configuring Amplify: $e");
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Authenticator(
+      child: MaterialApp(
+        title: 'Money Tomorrow',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        builder: Authenticator.builder(),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String? user;
+  String? result;
+
+  @override
+  void initState() {
+    super.initState();
+    checkUser();
+    fetchResult();
+  }
+
+  Future<void> checkUser() async {
+    print("fetching current signed in user");
+    var userObj = await Amplify.Auth.getCurrentUser();
+    var signInDetails = userObj.signInDetails.toJson();
+    var email = signInDetails['username'].toString();
+    print("email: $email");
+    setState(() {
+      user = email;
+    });
+  }
+
+  Future<void> fetchResult() async {
+    print("fetching data from API");
+    var apiRequest = Amplify.API.post('/router',
+        body: HttpPayload.json({'key1': '2', 'key2': '3'}),
+        apiName: 'Endpoint');
+    var apiResponse = await apiRequest.response;
+    print("response: ${apiResponse.decodeBody()}");
+    setState(() {
+      result = apiResponse.decodeBody();
+    });
+  }
+
+  Future<void> signOut() async {
+    print('signOut');
+    await Amplify.Auth.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Money Tomorrow'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello, ${user ?? 'Anonymous'}!'),
+            Text('2 + 3 = ${result ?? 'Unknown'}'),
+            ElevatedButton(
+              onPressed: () {
+                signOut();
+              },
+              child: Text('Sign Out'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
