10a7859860847ef9d253cccc8a73848ef24fe88d
Jared Franzone <jaredfranzone@Jareds-MacBook-Pro.local>
2023-08-14 20:19:13 -0700

    chore: parallelize API calls

--- lib/pages/DashboardPage.dart
+++ lib/pages/DashboardPage.dart
@@ -33,20 +33,16 @@ class _DashboardPageState extends State<DashboardPage> {
     getData();
   }
 
-  Future<void> getData() async {
-    List<Recurring> recurrings = await fetchRecurring();
-    List<OneTime> oneTimes = await fetchOneTime();
-    double startingBalance = await fetchAssets();
-    Settings settings = await fetchSettings();
+  MonteCarloServiceRequest getMonteCarloRequest(List<OneTime> oneTimes,
+      List<Recurring> recurrings, List<Asset> assets, Settings settings) {
     double mean =
         (settings.annualAssetReturnPercent - settings.annualInflationPercent);
-    MonteCarloService monteCarloService =
-        MonteCarloService(numberOfSimulations: 1000);
     int currAge = calculateAge(settings.birthday);
     int period = endAge - currAge;
     DateTime now = DateTime.now();
+    double startingBalance = Asset.computeTotalAssetValue(assets);
 
-    MonteCarloServiceRequest request = MonteCarloServiceRequest(
+    return MonteCarloServiceRequest(
         oneTimes: oneTimes,
         recurrings: recurrings,
         period: period,
@@ -57,6 +53,52 @@ class _DashboardPageState extends State<DashboardPage> {
         variance: sp500Variance,
         fees: FEES,
         numberOfSimulations: 1000);
+  }
+
+  Future<void> getData() async {
+    var recurringRequest =
+        Amplify.API.get('/recurring', apiName: 'Endpoint').response;
+    var oneTimeRequest =
+        Amplify.API.get('/onetime', apiName: 'Endpoint').response;
+    var assetsRequest =
+        Amplify.API.get('/assets', apiName: 'Endpoint').response;
+    var settingsRequest =
+        Amplify.API.get('/settings', apiName: 'Endpoint').response;
+    final responses = await Future.wait(
+        [recurringRequest, oneTimeRequest, assetsRequest, settingsRequest]);
+
+    // Recurring request
+    List<dynamic> recurringJson = jsonDecode(responses[0].decodeBody());
+    List<Recurring> recurrings = recurringJson.map((json) {
+      return Recurring.fromJson(json);
+    }).toList();
+
+    // One Time request
+    List<dynamic> oneTimeJson = jsonDecode(responses[1].decodeBody());
+    List<OneTime> oneTimes =
+        oneTimeJson.map((json) => OneTime.fromJson(json)).toList();
+
+    // Assets request
+    List<dynamic> assetJson = jsonDecode(responses[2].decodeBody());
+    List<Asset> assets = assetJson.map((json) => Asset.fromJson(json)).toList();
+    double totalValue = Asset.computeTotalAssetValue(assets);
+    var formatter = NumberFormat('#,##0.00', 'en_US');
+    String formattedTotalValue = formatter.format(totalValue);
+    String totalValueStr = '\$$formattedTotalValue';
+    setState(() {
+      startingBalanceStr = totalValueStr;
+    });
+
+    // Settings request
+    List<dynamic> settingJson = jsonDecode(responses[3].decodeBody());
+    List<Settings> allsettings =
+        settingJson.map((json) => Settings.fromJson(json)).toList();
+    Settings settings = allsettings[0];
+
+    MonteCarloService monteCarloService =
+        MonteCarloService(numberOfSimulations: 1000);
+    MonteCarloServiceRequest request =
+        getMonteCarloRequest(oneTimes, recurrings, assets, settings);
     MonteCarloServiceResponse response =
         monteCarloService.getMonteCarloResponse(request: request);
     List<PricePoint> newlin = response
@@ -79,55 +121,6 @@ class _DashboardPageState extends State<DashboardPage> {
     setState(() {
       user = email;
     });
-  }
-
-  Future<List<Recurring>> fetchRecurring() async {
-    var apiRequest = Amplify.API.get('/recurring', apiName: 'Endpoint');
-    var apiResponse = await apiRequest.response;
-    var jsonString = apiResponse.decodeBody();
-    List<dynamic> jsonList = jsonDecode(jsonString);
-
-    List<Recurring> recurrings = jsonList.map((json) {
-      return Recurring.fromJson(json);
-    }).toList();
-    return recurrings;
-  }
-
-  Future<List<OneTime>> fetchOneTime() async {
-    var apiRequest = Amplify.API.get('/onetime', apiName: 'Endpoint');
-    var apiResponse = await apiRequest.response;
-    var jsonString = apiResponse.decodeBody();
-    List<dynamic> jsonList = jsonDecode(jsonString);
-    List<OneTime> onetimes =
-        jsonList.map((json) => OneTime.fromJson(json)).toList();
-    return onetimes;
-  }
-
-  Future<double> fetchAssets() async {
-    var apiRequest = Amplify.API.get('/assets', apiName: 'Endpoint');
-    var apiResponse = await apiRequest.response;
-    var jsonString = apiResponse.decodeBody();
-
-    List<dynamic> jsonList = jsonDecode(jsonString);
-    List<Asset> assets = jsonList.map((json) => Asset.fromJson(json)).toList();
-    double totalValue = Asset.computeTotalAssetValue(assets);
-    var formatter = NumberFormat('#,##0.00', 'en_US');
-    String formattedTotalValue = formatter.format(totalValue);
-    String totalValueStr = '\$$formattedTotalValue';
-    setState(() {
-      startingBalanceStr = totalValueStr;
-    });
-    return totalValue;
-  }
-
-  Future<Settings> fetchSettings() async {
-    var apiRequest = Amplify.API.get('/settings', apiName: 'Endpoint');
-    var apiResponse = await apiRequest.response;
-    var jsonString = apiResponse.decodeBody();
-    List<dynamic> jsonList = jsonDecode(jsonString);
-    List<Settings> settings =
-        jsonList.map((json) => Settings.fromJson(json)).toList();
-    return settings[0];
   }
 
   Future<void> signOut() async {
