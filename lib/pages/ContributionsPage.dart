0d08c37d5a942069891f123665a45be3ede34664
Jared Franzone <jaredfranzone@Jareds-MacBook-Pro.local>
2023-08-31 19:43:32 -0700

    feat: contributions page

--- lib/pages/ContributionsPage.dart
+++ lib/pages/ContributionsPage.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:moneyapp_flutter/model/recurring.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:moneyapp_flutter/model/charge_type.dart';
 
 class ContributionsPage extends StatefulWidget {
   const ContributionsPage({super.key});
@@ -8,20 +13,58 @@ class ContributionsPage extends StatefulWidget {
 }
 
 class _ContributionsPageState extends State<ContributionsPage> {
+  List<Recurring> contributions = [];
+
   @override
   void initState() {
     super.initState();
+    getData();
+  }
+
+  Future<void> getData() async {
+    var getRecurringData = Amplify.API.get('/listRecurring',
+        apiName: 'Endpoint', queryParameters: {"scenarioId": "s1"});
+    var listRecurringResponse = await getRecurringData.response;
+    var getScenarioDataJSON = listRecurringResponse.decodeBody();
+    List<dynamic> scenarioDataJSON = jsonDecode(getScenarioDataJSON);
+
+    // Assets
+    List<Recurring> recurrings =
+        scenarioDataJSON.map((json) => Recurring.fromJson(json)).toList();
+
+    List<Recurring> contributions = recurrings
+        .where((recurring) => recurring.chargeType == ChargeType.INCOME)
+        .toList();
+
+    setState(() {
+      this.contributions = contributions;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('Contributions Page')],
+    if (contributions.isEmpty) {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
         ),
-      ),
-    );
+      );
+    } else {
+      return Scaffold(
+        body: ListView(
+          children: contributions.map((withdrawal) {
+            String result =
+                withdrawal.lineItems.map((item) => item.toString()).join(', ');
+            return Card(
+              child: ListTile(
+                title: Text(
+                    '${withdrawal.title} (${withdrawal.startAge} - ${withdrawal.endAge}), ${result}'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            );
+          }).toList(),
+        ),
+      );
+    }
   }
 }
